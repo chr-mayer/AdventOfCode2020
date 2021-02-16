1bef0bad2278a7a8200457f25bb9ce3120d8f29f
chrm <christof.mayer@gmail.com>
2021-02-16 15:21:54 +0100

    cl

--- src/day14/Part2.java
+++
@@ -1,9 +0,0 @@
-package day14;
-
-public class Part2 {
-    public static void main(String[] args) {
-        Emulator emulator = new Emulator();
-        long sum = emulator.processInstructions2();
-        System.out.println(sum);
-    }
-}
